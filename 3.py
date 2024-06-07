5ed46bedbefeb4d81b19d5d2f83778765e1f6797
bupt-2917 <khrdsg@163.com>
2024-06-07 16:24:56 +0800

    6.7

---
+++ 3.py
@@ -0,0 +1,8 @@
+bikes=['trek','redline','giant']
+print(bikes)
+print(bikes[0])
+print(bikes[2],bikes[-1])
+
+bicyckes=['trek','cannondale','redline','specialized']
+message=f"My first bicycle was a +{bicyckes[0].title()}+"
+
