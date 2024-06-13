b6d1276c66314b3cc3fab4a6d7194b7b729b6481
bupt-2917 <khrdsg@163.com>
2024-06-13 11:09:42 +0800

    hello

--- 函数.py
+++ 函数.py
@@ -68,3 +68,59 @@
 #         break
 #     formatted_name=get_formatted_name(f_name,l_name)
 #     print(f"\nHello, {formatted_name}!")
+
+
+# def greet_users(names):
+#     """向列表中的每位用户都发出简单的问候"""
+#     for name in names:
+#         msg=f"Hello, {name.title()}!"
+#         print(msg)
+
+# usernames=['hannah','ty','margot']
+# greet_users(usernames)
+
+# def verify_users(unconfirmed_users,confirmed_users):
+#     """验证用户直到没有未验证用户为止，将每个经过验证的用户都移到已验证用户列表中"""
+#     while unconfirmed_users:
+#         current_user=unconfirmed_users.pop()
+#         print(f"Verifying user: {current_user.title()}")
+#         confirmed_users.append(current_user.title())
+# def show_confirmed_users(confirmed_users):
+#     """显示所有已验证用户"""
+#     print("\nThe following users have been confirmed:")
+#     for confirmed_user in confirmed_users:
+#         print(confirmed_user)
+
+# unconfirmed_users=['alice','brian','candace']
+# confirmed_users=[]
+# verify_users(unconfirmed_users[:],confirmed_users)
+# show_confirmed_users(confirmed_users)
+# print(unconfirmed_users)
+
+# def make_pizza(*toppings):
+#     """打印顾客点的所有配料"""
+#     print("\nMaking a pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f"- {topping}")
+# make_pizza('pepperoni')
+# make_pizza('mushrooms','green peppers','extra cheese')
+
+# def make_pizza(size,*toppings):
+#     """概述要制作的披萨"""
+#     print(f"\nMaking a {size}-inch pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f"- {topping}")
+
+# make_pizza(16,'pepperoni')
+# make_pizza(12,'mushrooms','green peppers','extra cheese')
+
+def build_profile(first,last,**user_info):
+    """创建一个字典，其中包含我们知道的有关用户的一切"""
+    profile={}
+    profile['first_name']=first
+    profile['last_name']=last
+    for key,value in user_info.items():
+        profile[key]=value
+    return profile
+user_profile=build_profile('albert','einstein',location='princeton',field='physics')
+print(user_profile)
