0e264697a4d96c0a94593f117ff93e969e4ade7e
bupt-2917 <khrdsg@163.com>
2024-06-14 11:45:35 +0800

    Refactor Car class to include _init_ method and increment_odometer function

--- 类.py
+++ 类.py
@@ -26,29 +26,41 @@ class Dog:
 # print(f"Your dog is {your_dog.age} years old.")
 # your_dog.sit()
 
-# class Car:
-#     """模拟汽车"""
-#     def __init__(self,make,model,year):
-#         """初始化描述汽车的属性"""
-#         self.make=make
-#         self.model=model
-#         self.year=year
-#         self.odometer_reading=0
-#     def get_description_name(self):
-#         """返回整洁的描述信息"""
-#         long_name=f"{self.year} {self.make} {self.model}"
-#         return long_name.title()
-#     def read_odometer(self):
-#         """打印一条指出汽车里程的消息"""
-#         print(f"This car has {self.odometer_reading} miles on it.")
+class Car:
+    """模拟汽车"""
+    def __init__(self,make,model,year):
+        """初始化描述汽车的属性"""
+        self.make=make
+        self.model=model
+        self.year=year
+        self.odometer_reading=0
+    def _init_(self,make,model):
+        """初始化描述汽车的属性"""
+        self.make=make
+        self.model=model
+        self.odometer_reading=0
+    def get_description_name(self):
+        """返回整洁的描述信息"""
+        long_name=f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    def read_odometer(self):
+        """打印一条指出汽车里程的消息"""
+        print(f"This car has {self.odometer_reading} miles on it.")
 
-#     def update_odometer(self,mileage):
-#         """将里程表读数设置为指定的值"""
-#         if mileage>=self.odometer_reading:
-#             self.odometer_reading=mileage
-#         else:
-#             print("You can't roll back an odometer!")
+    def update_odometer(self,mileage):
+        """将里程表读数设置为指定的值"""
+        if mileage>=self.odometer_reading:
+            self.odometer_reading=mileage
+        else:
+            print("You can't roll back an odometer!")
+    def increment_odometer(self,miles):
+        """将里程表读数增加指定的量"""
+        self.odometer_reading+=miles
 # my_new_car=Car('audi','a4',2019)   
+# my_new_car.update_odometer(23_500)
+# my_new_car.read_odometer()
+# my_new_car.increment_odometer(100)
+# my_new_car.read_odometer()
 # print(my_new_car.get_description_name())
 # my_new_car.read_odometer()
 
@@ -57,3 +69,28 @@ class Dog:
 
 # my_new_car.update_odometer(22)
 # my_new_car.read_odometer()
+
+
+class ElctricCar(Car):
+    """电动车的独特之处"""
+    def __init__(self,make,model,year):
+        """初始化父类的属性"""
+        super().__init__(make,model,year)
+        self.battery_size=75
+    def describe_battery(self):
+        """打印一条描述电瓶容量的消息"""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+    def fill_gas_tank(self):
+        """电动车没有油箱"""
+        print("This car doesn't need a gas tank!")
+    def get_description_name(self):
+        """返回整洁的描述信息"""
+        long_name=f"{self.year} {self.make} {self.model}{self.battery_size}"
+        return long_name.title()
+
+# my_tesla=ElctricCar('tesla','model s',2019)
+# print(my_tesla.get_description_name())
+# my_tesla.describe_battery()
+# print(my_tesla.get_description_name())
+
+class batt
