cfb0b92899cdc29b77a44740fb697f8b1d10b9f0
bupt-2917 <khrdsg@163.com>
2024-06-17 21:35:07 +0800

    Refactor file_reader.py to read and concatenate lines from pi_digits.txt

--- module-class-demo/file-demo/file_reader.py
+++ module-class-demo/file-demo/file_reader.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 
+path = Path('D:/python_STUDY/python/module-class-demo/file-demo/pi_digits.txt')
+contents = path.read_text()
+lines=contents.splitlines()
+pi_line=""
+for line in lines:
+    pi_line+=line.strip()
 
-path=Path('file-demo/pi_digits.txt')
-contents=path.read_text()
-print(contents)
+print(pi_line)
+print(len(pi_line))
